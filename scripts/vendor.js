3bc0de82704e7ea97f4116c4be17fc1b537f6739
Jordan Lee <jtlee@rmgj2.com>
2023-01-27 19:10:15 -0600

    Co-authored-by: jacksonxlee <jacksonxlee@users.noreply.github.com> Co-authored-by: cameron <clscmrn@users.noreply.github.com>

---
+++ scripts/vendor.js
@@ -0,0 +1,11 @@
+const plantInfo = [
+    {name: 'lettuce', ph: 5.3, ec: 1.8, growthTime: '2 weeks', temperature: 72, img: 'pictures/'},
+    {name: 'carrots', ph: '6 - 6.3', ec: 0, growthTime: ' 11 weeks', temperature: 65, img: 'pictures/'},
+    {name: 'rainbow shard', ph: 0, ec: 0, growthTime: ' weeks', temperature: 0, img: 'pictures/'},
+    {name: '', ph: 0, ec: 0, growthTime: ' weeks', temperature: 0, img: 'pictures/'},
+    {name: '', ph: 0, ec: 0, growthTime: ' weeks', temperature: 0, img: 'pictures/'},
+    {name: '', ph: 0, ec: 0, growthTime: ' weeks', temperature: 0, img: 'pictures/'},
+    {name: '', ph: 0, ec: 0, growthTime: ' weeks', temperature: 0, img: 'pictures/'},
+    {name: '', ph: 0, ec: 0, growthTime: ' weeks', temperature: 0, img: 'pictures/'},
+    {name: '', ph: 0, ec: 0, growthTime: ' weeks', temperature: 0, img: 'pictures/'},
+]
