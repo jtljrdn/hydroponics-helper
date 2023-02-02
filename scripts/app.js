3498a3e42499945bbcc490aeca3bbf1cf705ad68
Jordan Lee <jtlee@rmgj2.com>
2023-02-02 10:55:48 -0600

    update

--- scripts/app.js
+++ scripts/app.js
@@ -16,8 +16,14 @@ function fillInfo(plantChoice) {
         for (let i = 0; i < plantInfo.length; i++) {
             if (plantChoice == plantInfo[i].name) {
             console.log(plantInfo[i].name, plantChoice)
+            infoTable.style.display = 'inline-block'
             plantImg.src = plantInfo[i].img
             plantName.innerHTML = plantInfo[i].name
+            plantType.innerHTML = plantInfo[i].type
+            plantPH.innerHTML = `Recommended PH: ${plantInfo[i].ph}`
+            plantEC.innerHTML = `Recommended E.C.: ${plantInfo[i].ec}`
+            plantTemp.innerHTML = `Average Recommended Temperature: ${plantInfo[i].temperature}&#8457;`
+            plantGrowthTime.innerHTML = `Expected Growth Time: ${plantInfo[i].growthTime}`
             }
         }
     }
