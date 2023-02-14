32e886a52908e39586cdb262982b0584eb47a24e
Jordan Lee <jtlee@rmgj2.com>
2023-02-14 10:23:38 -0600

    formatted

--- scripts/app.js
+++ scripts/app.js
@@ -10,29 +10,31 @@ function fillDropdown() {
 }
 
 function fillInfo(plantChoice) {
-    console.log(plantChoice)
-    if (plantChoice != '') {
-        resetInfo()
-        for (let i = 0; i < plantInfo.length; i++) {
-            if (plantChoice == plantInfo[i].name) {
-            console.log(plantInfo[i].name, plantChoice)
-            infoTable.style.display = 'inline-block'
-            plantImg.src = plantInfo[i].img
-            plantName.innerHTML = `<strong>${plantInfo[i].name}</strong>`
-            plantType.innerHTML = `<em>${plantInfo[i].type}</em>`
-            plantPH.innerHTML = `Recommended PH: ${plantInfo[i].ph}`
-            plantEC.innerHTML = `Recommended E.C.: ${plantInfo[i].ec}`
-            plantTemp.innerHTML = `Average Recommended Temperature: ${plantInfo[i].temperature}&#8457;`
-            plantGrowthTime.innerHTML = `Expected Growth Time: ${plantInfo[i].growthTime}`
-            }
-        }
-    } else {
-      alert('Please Choose a Plant')
+  // fills the info table with the selected plant's info
+  console.log(plantChoice);
+  if (plantChoice != "") {
+    resetInfo();
+    for (let i = 0; i < plantInfo.length; i++) {
+      if (plantChoice == plantInfo[i].name) {
+        console.log(plantInfo[i].name, plantChoice);
+        infoTable.style.display = "inline-block";
+        plantImg.src = plantInfo[i].img;
+        plantName.innerHTML = `<strong>${plantInfo[i].name}</strong>`;
+        plantType.innerHTML = `<em>${plantInfo[i].type}</em>`;
+        plantPH.innerHTML = `Recommended PH: ${plantInfo[i].ph}`;
+        plantEC.innerHTML = `Recommended E.C.: ${plantInfo[i].ec}`;
+        plantTemp.innerHTML = `Average Recommended Temperature: ${plantInfo[i].temperature}&#8457;`;
+        plantGrowthTime.innerHTML = `Expected Growth Time: ${plantInfo[i].growthTime}`;
+      }
     }
+  } else {
+    alert("Please Choose a Plant");
+  }
 }
 
 function calculateEC(currentEC, currentPlant) {
-  EcResult.innerHTML = '';
+  // calculates the amount of cups of NPK mixture to add to the water
+  EcResult.innerHTML = "";
   for (let i = 0; i < plantInfo.length; i++) {
     if (currentPlant == plantInfo[i].name) {
       currentPlant = plantInfo[i];
@@ -45,23 +47,23 @@ function calculateEC(currentEC, currentPlant) {
         cupsToAddFixed = cupsToAdd.toFixed(2);
         EcResult.innerHTML = `Add ${cupsToAddFixed} cups of NPK mixture to your water for ${currentPlant.name}.`;
       } else if (currentEC == currentPlant.ec) {
-        alert("Your E.C. is already high enough");
+        EcResult.innerHTML = "Your E.C. is already high enough";
       } else if (currentEC > currentPlant.ec) {
-        alert("Your E.C. is too high, add water to mixture.");
+        EcResult.innerHTML = "Your E.C. is too high, add water to mixture.";
       }
     }
   }
 }
 
-
-function resetInfo(){
-    plantImg.src = '';
-    plantName.innerHTML = '';
-    plantType.innerHTML = '';
-    plantPH.innerHTML = '';
-    plantEC.innerHTML = '';
-    plantTemp.innerHTML ='';
-    plantGrowthTime.innerHTML = '';
-    currentEC.value = '';
-    EcResult.innerHTML = '';
+function resetInfo() {
+  // resets the info table
+  plantImg.src = "";
+  plantName.innerHTML = "";
+  plantType.innerHTML = "";
+  plantPH.innerHTML = "";
+  plantEC.innerHTML = "";
+  plantTemp.innerHTML = "";
+  plantGrowthTime.innerHTML = "";
+  currentEC.value = "";
+  EcResult.innerHTML = "";
 }
